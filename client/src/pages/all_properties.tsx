cbc8150b091c0f389b1b77eb4b4620b66bca33db
victor prince <mumo.victor@strathmore.edu>
2023-04-17 17:50:10 +0300

    updated commit of mern app

---
+++ client/src/pages/all_properties.tsx
@@ -0,0 +1,42 @@
+import { Add } from "@mui/icons-material";
+import { useTable } from "@pankod/refine-core";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@pankod/refine-mui";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import { useMemo } from "react";
+
+import { PropertyCard, CustomButton } from "components";
+
+const AllProperties = () => {
+  const navigate = useNavigate();
+
+  return(
+  <Box>
+      <Stack direction="row"
+              justifyContent="space-between"
+              alignItems="center">
+      <Typography fontSize={25}
+                  fontWeight={700}
+                  color="#11142d">All Properties
+                   </Typography>
+      <CustomButton 
+      title="Add Property"
+      handleClick={() => navigate('/properties/create')}
+      backgroundColor="#475be8"
+      color="#fcfcfc"
+      icon={<Add />}
+      
+      />
+    </Stack>
+
+  </Box>
+  );
+}
+
+export default AllProperties
